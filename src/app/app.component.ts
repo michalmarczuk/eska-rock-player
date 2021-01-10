32230c4465b78f8c43ede12b83c3ee2919825716
Michał Marczuk <michal.marczuk@globalapptesting.com>
2021-01-10 11:45:46 +0100

    Added simple stream player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('player') player: ElementRef<HTMLAudioElement>; 
   title = 'eska-rock-player';
+  status = 'pause';
+
+  onButtonCLick() {
+    this.status = this.status === 'play' ? 'pause' : 'play';
+    this.player.nativeElement[this.status]();
+  }
 }
