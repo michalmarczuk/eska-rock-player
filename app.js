063ded4592183302a00ae3084ce0ae9d66a48f30
Michał Marczuk <michal.marczuk@globalapptesting.com>
2021-03-04 16:43:44 +0100

    Working beta version

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const express = require('express');
+const request = require('request');
+const expressApp = express();
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 955,
-    height: 489,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    resizable: false
-  });
-  
-  win.loadURL('http://localhost:4200');
-  // win.webContents.openDevTools();
+// Start server
+expressApp.use(express.static(__dirname + '/dist'));
+
+expressApp.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+expressApp.get('/eska_api/combine.jsonp', function (req, res) {
+    var newurl = 'https://static.eska.pl/m/playlist/combine.jsonp?callback=jsonp';
+    request(newurl).pipe(res);
+});
+
+expressApp.listen(process.env.PORT || 4200);
+
+// Open Electron
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 955,
+        height: 489,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false
+    });
+
+    mainWindow.loadURL('http://localhost:4200');
+    // mainWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     app.quit()
